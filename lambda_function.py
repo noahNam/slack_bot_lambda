a3337b479d3d68b76d971cd6b2b3bb16fecbde26
noah <noah85.Nam@gmail.com>
2021-05-21 21:44:53 +0900

    add requests module

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,17 @@
+from lib import requests
+
+
+def post_message(token, channel, text):
+    requests.post("https://slack.com/api/chat.postMessage",
+                  headers={"Authorization": "Bearer " + token},
+                  data={"channel": channel, "text": text}
+                  )
+
+
 def lambda_handler():
     full_msg = "test 입니다"
     slack_token = "xoxb-1234"
+    post_message(slack_token, "#bot-test", full_msg)
 
 
 if __name__ == '__main__':
